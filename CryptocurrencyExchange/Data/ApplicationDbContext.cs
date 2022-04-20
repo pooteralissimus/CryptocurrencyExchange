d89a77bcc6026b35383c1cc4ec7e1f3bf732fd93
supalapka <yurabalint0103@gmail.com>
2022-04-20 18:05:25 +0300

    Integrated EF5

--- CryptocurrencyExchange/Data/ApplicationDbContext.cs
+++ CryptocurrencyExchange/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DbAccessLibrary.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;

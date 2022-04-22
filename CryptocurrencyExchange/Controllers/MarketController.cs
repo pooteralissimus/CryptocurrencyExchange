1b8b4c74b8a887fe5ae191f0613dade8796b135a
supalapka <yurabalint0103@gmail.com>
2022-04-22 20:00:40 +0300

    Added MarketController

---
+++ CryptocurrencyExchange/Controllers/MarketController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CryptocurrencyExchange.Controllers
+{
+    public class MarketController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            string json;
+            using (var web = new System.Net.WebClient())
+            {
+                var url = @"https://api.coindesk.com/v1/bpi/currentprice.json";
+                json = web.DownloadString(url);
+            }
+
+            dynamic obj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            var currentPrice = Convert.ToDecimal(obj.bpi.USD.rate.Value);
+            Console.WriteLine(currentPrice);
+
+            return View(currentPrice);
+        }
+    }
+}

d9bc5a07c1c1b6dc0d681e7ea99884649e0f4e43
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-04-24 00:38:22 +0300

    save open day price

--- ClassLibrary/CryptocurrencyOperations.cs
+++ ClassLibrary/CryptocurrencyOperations.cs
@@ -1,4 +1,6 @@
 ﻿using DbAccessLibrary.DataAccess;
+using DbAccessLibrary.Models;
+using System;
 using System.Collections.Generic;
 
 namespace ClassLibrary
@@ -6,7 +8,7 @@ namespace ClassLibrary
     public static class CryptocurrencyOperations
     {
 
-        public static List<dynamic> GetPrices(string[] coinsName)
+        public static List<dynamic> GetPrices(string[] coinsName,MyDbContext ctx)
         {
             List<dynamic> coinsPrice = new List<dynamic>();
             string json;
@@ -21,17 +23,43 @@ namespace ClassLibrary
 
                 dynamic coinInfo = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
                 coinsPrice.Add(coinInfo);
-
             }
-            return coinsPrice;
+
+            var date = DateTime.Now; //check for day open to save prices
+            if (date.Hour == 0) {
+                DayOpenCheck(coinsPrice, ctx);
+            }
+
+                return coinsPrice;
 
         }
 
-        public static double Get24hChanges(string coinName,MyDbContext ctx)
+        public static double Get24hChanges(string coinName, MyDbContext ctx)
         {
             double result = 0;
 
             return result;
+        }
+
+        public static void DayOpenCheck(List<dynamic> coinsPrice, MyDbContext ctx) //fix later
+        {
+            var date = DateTime.Now;
+            if (date.Hour == 0)
+            {
+                var deletes = ctx.OpenPrices;
+               foreach(var delete in deletes)
+                    ctx.OpenPrices.Remove(delete);
+
+
+                foreach (var coin in coinsPrice)
+                {
+                    string coinname = coin.symbol;
+                    coinname = coinname.Remove(coinname.Length - 4);
+                    var openPrice = new CryptoOpenPrices() { CoinName = coinname, OpenPrice = Convert.ToDecimal(coin.price) };
+                    ctx.OpenPrices.Add(openPrice);
+                }
+                 ctx.SaveChanges();
+            }
         }
     }
 
--- CryptocurrencyExchange/Controllers/MarketController.cs
+++ CryptocurrencyExchange/Controllers/MarketController.cs
@@ -14,12 +14,13 @@ namespace CryptocurrencyExchange.Controllers
 
         public IActionResult Index()
         {
+
             string[] coinsName = { "BTC", "ETH", "SOL", "LUNA","BNB",
                 "XRP", "DOGE", "SHIB", "AXS","MANA", "LTC", "ATOM" };
 
             List<CoinOutputModel> outputCoins = new List<CoinOutputModel>();
 
-            var prices = CryptocurrencyOperations.GetPrices(coinsName);
+            var prices = CryptocurrencyOperations.GetPrices(coinsName,_context);
 
             //foreach (var price in prices)
             //{

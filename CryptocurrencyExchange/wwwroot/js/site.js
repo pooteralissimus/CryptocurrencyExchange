517f07af4971a13fd8b838ca536081f9a5bfcfb6
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-04-24 11:58:21 +0300

    percent  sign fixes

--- CryptocurrencyExchange/wwwroot/js/site.js
+++ CryptocurrencyExchange/wwwroot/js/site.js
@@ -1,8 +1,8 @@
-﻿
-$(".row-crypto-table__percent_red").each(function () {
+﻿$(".row-crypto-table__percent_red").each(function () {
     var changes24h = $($(this).contents()[1]).text();
     if (parseFloat(changes24h) >= 0) {
-        $(this).removeClass("row-crypto-table__percent_red");
-        $(this).addClass("row-crypto-table__percent_green");
+        $(this).removeClass("row-crypto-table__percent_red")
+        $(this).addClass("row-crypto-table__percent_green")
+        $(this).text("+" + changes24h+"%")
     }
 })

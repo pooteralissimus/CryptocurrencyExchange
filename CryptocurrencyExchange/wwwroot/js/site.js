c5ae17c6c9508434641a6f61d5a72e605a284963
Yura Balint <46222135+supalapka@users.noreply.github.com>
2022-04-24 03:32:51 +0300

    script that change 24hchanges percent color

--- CryptocurrencyExchange/wwwroot/js/site.js
+++ CryptocurrencyExchange/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿
+$(".row-crypto-table__percent_red").each(function () {
+    var changes24h = $($(this).contents()[1]).text();
+    if (parseFloat(changes24h) >= 0) {
+        $(this).removeClass("row-crypto-table__percent_red");
+        $(this).addClass("row-crypto-table__percent_green");
+    }
+})
